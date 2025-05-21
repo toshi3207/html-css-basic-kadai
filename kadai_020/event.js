631133777a805099f07381d5f5a1d95ea2e117e0
toshi3207 <guiltycrown2139@gmail.com>
2025-05-22 01:26:47 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+const text = document.getElementById('text');
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  text.textContent = 'ボタンをクリックしました';
+});
+
+console.log(text);
