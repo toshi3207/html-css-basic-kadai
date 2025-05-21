c6f439e1bb2b45125f5076d58503f52654f9c54c
toshi3207 <guiltycrown2139@gmail.com>
2025-05-22 01:26:30 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,9 +0,0 @@
-const text = document.getElementById('text');
-
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
-  text.textContent = 'ボタンをクリックしました';
-});
-
-console.log(text);
